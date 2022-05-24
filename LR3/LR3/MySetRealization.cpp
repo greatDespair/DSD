85dcad3778934d61a38f8093db13cc366fb8950c
Sergey <akmashev2002@gmail.com>
2022-05-24 16:31:44 +0300

    LR4 complete for test

--- LR3/LR3/MySetRealization.cpp
+++ LR3/LR3/MySetRealization.cpp
@@ -53,20 +53,28 @@ int MySet::SetPower() {
 // Создание нового множества
 void MySet::generateSet(int count, int min, int max, int lastDigit) {
         int i = 0;
-     
+        Element* check;
         if (lastDigit == 3) {
-            while (SetPower() < count) {
+            while (i < count) {
+                check = start;
                 int temp = rand() % (max - min + 1) + min;
-                if (temp % 10 > 3)
+                if (temp % 10 > 3) {
                     add(temp);
+                    if (start != check)
+                        i++;
+                }
             }
         }
         else
             if (lastDigit == 8)
-                while (SetPower() < count) {
+                while (i < count) {
+                    check = start;
                     int temp = rand() % (max - min + 1) + min;
-                    if (temp % 10 < 8)
+                    if (temp % 10 < 8) {
                         add(temp);
+                        if (start != check)
+                            i++;
+                    }
                 }
             else
                 cout << "Ошибка добавления" << endl;
@@ -87,9 +95,9 @@ string MySet::printSet() {
 // Удаление множества
 void MySet::deleteSet() {
     Element* current = start;
-    if (this->isEmpty())                   // Если список пуст, сообщить об этом
+    if (this->isEmpty())                   
         cout << "Нет удаляемых элементов множества" << endl;
-    while (current) {                       // Удаление элементов, если они есть
+    while (current) {                       
         Element* temp = current;
         current = current->Next;
         delete temp;
@@ -162,188 +170,3 @@ MySet MySet::symDiffOfSets(MySet a, MySet b) {
     if (sameOfSets(a, b).isEmpty()) return mergeSets(a, b);
     return diffOfSets(mergeSets(a, b), sameOfSets(a, b));
 }
-
-
-//
-//MySet::MySet() {
-//	start.Value = NULL;
-//    start.Next = nullptr;
-//}
-//
-//MySet::~MySet() {
-//    Element* current = &start;
-//    if (isEmpty())
-//        cout << "Множество пустое!" << endl;
-//    while (current) {
-//        Element* temp = current;
-//        current = current->Next;
-//        delete temp;
-//    }
-//    delete &start;
-//}
-//
-//bool MySet::isEmpty() {
-//    return !&start;
-//}
-//
-//bool MySet::SetCheck(int value) {
-//    if (isEmpty()) 
-//        return false;
-//    Element* current = &start;
-//    while (current) {
-//        if (current->Value == value)
-//            return true;
-//        current = current->Next;
-//    }
-//    return false;
-//}
-//
-//bool MySet::add(int value) {
-//    if (!SetCheck(value)) {
-//        
-//        Element* new_node = new Element;
-//        new_node->Value = start.Value;
-//        new_node->Next = start.Next;
-//        start.Value = value;
-//        start.Next = new_node;
-//        return true;
-//    }
-//    return false;
-//}
-//
-//int MySet::SetPower() {
-//    int power = 1;
-//    if (isEmpty()) return 0;
-//    Element* current = &start;
-//    while (current->Next) {
-//        current = current->Next;
-//        power++;
-//    }
-//    return power;
-//}
-//
-//void MySet::generateSet(int count, int min, int max, int lastDigit) {
-//
-//    int i = 0;
-//
-//    if (lastDigit == 3) {
-//        while (i < count) {
-//            int temp = rand() % (max - min + 1) + min;
-//            if (temp % 10 > 3)
-//                if (add(temp))
-//                    i++;
-//        }
-//    }
-//    else
-//        if (lastDigit == 8)
-//            while (i < count) {
-//                int temp = rand() % (max - min + 1) + min;
-//                if (temp % 10 < 8)
-//                    if (add(temp))
-//                        i++;
-//            }
-//        else
-//            cout << "Ошибка добавления" << endl;
-//}
-//
-//string MySet::printSet() {
-//    if (isEmpty()) return "Пустое множество";
-//    Element* current = &start;
-//    string print;
-//    while (current) {
-//        print += to_string(current->Value) + " ";
-//        current = current->Next;
-//    }
-//    return print;
-//}
-//
-//bool MySet::isSubset(MySet SetA, MySet SetB) {
-//    cout << "Множество А: " << SetA.printSet() << endl;
-//    cout << "Множество В: " << SetB.printSet() << endl;
-//
-//    if (SetA.isEmpty()) {
-//        return false;
-//    }
-//
-//    if (SetA.SetPower() > SetB.SetPower()) {
-//        return false;
-//    }
-//
-//    Element* current = &SetA.start;
-//    while (current->Next) {
-//        if (!SetB.SetCheck(current->Value)) {
-//            return false;
-//        }
-//        current = current->Next;
-//    }
-//    return true;
-//}
-//
-//bool MySet::isEqual(MySet A, MySet B) {
-//    return isSubset(A, B) && isSubset(B, A);
-//}
-//
-//MySet MySet::mergeSets(MySet A, MySet B) {
-//    if (A.isEmpty() || B.isEmpty())
-//        return MySet();
-//    MySet C = MySet();
-//    Element* current = &A.start;
-//    while (current)
-//    {
-//        C.add(current->Value);
-//        current = current->Next;
-//    }
-//
-//    current = &B.start;
-//    while (current)
-//    {
-//        if (!C.SetCheck(current->Value)) {
-//            C.add(current->Value);
-//        }
-//        current = current->Next;
-//    }
-//
-//    return C;
-//}
-//
-//MySet MySet::sameOfSets(MySet A, MySet B){
-//    if (A.isEmpty() || B.isEmpty())
-//        return MySet();
-//
-//    MySet C = MySet();
-//    Element* current = &A.start;
-//
-//    while (current) {
-//        if (B.SetCheck(current->Value))
-//            C.add(current->Value);
-//        current = current->Next;
-//    }
-//
-//    return C;
-//}
-//
-//MySet MySet::diffOfSets(MySet A, MySet B) {
-//    if (A.isEmpty() || B.isEmpty())
-//        return MySet();
-//
-//    MySet C = MySet();
-//    Element* current = &A.start;
-//
-//    while (current)
-//    {
-//        if (!B.SetCheck(current->Value))
-//            C.add(current->Value);
-//        current = current->Next;
-//    }
-//
-//    return C;
-//}
-//
-//MySet MySet::symDiffOfSets(MySet A, MySet B) {
-//
-//    if (sameOfSets(A, B).isEmpty())
-//        return mergeSets(A, B);
-//
-//    return diffOfSets(mergeSets(A, B), sameOfSets(A, B));
-//}
-//
---
+++ LR4/LR4/Set_Cont_List_Akmashev.cpp
@@ -0,0 +1,8 @@
+#include <iostream>;
+#include "SetLab4_Akmashev.h";
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+}
